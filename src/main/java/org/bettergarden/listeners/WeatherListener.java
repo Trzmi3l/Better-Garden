1d6b2405e44821a0bbcf9ba347e434d24b127508
Trzmi3l <76966391+Trzmi3l@users.noreply.github.com>
2023-08-06 22:06:22 +0200

    init

---
+++ src/main/java/org/bettergarden/listeners/WeatherListener.java
@@ -0,0 +1,28 @@
+package org.bettergarden.listeners;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class WeatherListener {
+
+    public WeatherListener() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerTick(TickEvent.LevelTickEvent event) {
+        if(event.level.isRaining()) {
+            // TODO: cropy przyspieszaja jesli pada deszcz
+
+            Level world = event.level;
+            //BlockPos playerPos = event.world.getClosestPlayer(event.world.getSpawnPoint().getX(), event.world.getSpawnPoint().getY(), event.world.getSpawnPoint().getZ(), -1, false).getPosition();
+
+
+        }
+    }
+
+}
